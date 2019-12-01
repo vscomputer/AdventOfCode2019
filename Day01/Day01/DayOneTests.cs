a6fc65ab83cdaff6b82ca42a9fcd36c2c7329287
Clint Hoagland <bachelor.one@gmail.com>
2019-12-01 09:45:34 -0500

    New Rider Solution

---
+++ Day01/Day01/DayOneTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Day01
+{
+    [TestFixture]
+    public class DayOneTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
