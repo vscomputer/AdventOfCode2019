95b1a9a6a698f621e74863718dd76c8a16babc0d
Clint Hoagland <bachelor.one@gmail.com>
2019-12-01 10:50:27 -0500

    solved Part Two

--- Day01/Day01/DayOneTests.cs
+++ Day01/Day01/DayOneTests.cs
@@ -26,14 +26,14 @@ namespace Day01
         public void Calculate_MassIs1969_FuelIs654()
         {
             var subject = new CalculatesFuelFromMass();
-            subject.Calculate(1969).Should().Be(654);
+            subject.Calculate(1969).Should().Be(966);
         }
 
         [Test]
         public void Calculate_MassIs100756_FuelIs33583()
         {
             var subject = new CalculatesFuelFromMass();
-            subject.Calculate(100756).Should().Be(33583);
+            subject.Calculate(100756).Should().Be(50346);
         }
 
         [Test]
@@ -47,7 +47,8 @@ namespace Day01
         public void ParseFile_RealInput_FuelIsTheAnswer()
         {
             var subject = new CalculatesFuelFromMass();
-            subject.ParseFile("C:\\Projects\\Homework\\AdventOfCode2019\\Day01\\input.txt").Should().Be(3173518);
+            // subject.ParseFile("C:\\Projects\\Homework\\AdventOfCode2019\\Day01\\input.txt").Should().Be(3173518); //Part One answer
+            subject.ParseFile("C:\\Projects\\Homework\\AdventOfCode2019\\Day01\\input.txt").Should().Be(4757427);
         }
     }
 
@@ -55,7 +56,13 @@ namespace Day01
     {
         public decimal Calculate(decimal mass)
         {
-            return Math.Floor(mass / 3) - 2;
+            decimal result = 0;
+            result += Math.Floor(mass / 3) - 2;
+            if (Math.Floor(result / 3) - 2 > 0)
+            {
+                result += Calculate(result);
+            }
+            return result;
         }
 
         public decimal ParseFile(string fileName)
