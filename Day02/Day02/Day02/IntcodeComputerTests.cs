7c5e142e09d63783c42aaa51a42393f40f6438bb
Clint Hoagland <bachelor.one@gmail.com>
2019-12-02 12:32:14 -0500

    refactor tests

--- Day02/Day02/Day02/IntcodeComputerTests.cs
+++ Day02/Day02/Day02/IntcodeComputerTests.cs
@@ -8,22 +8,28 @@ namespace Day02
     [TestFixture]
     public class IntcodeComputerTests
     {
+        private IntcodeComputer _subject;
+
+        [SetUp]
+        public void InitTests()
+        {
+            _subject = new IntcodeComputer();
+        }
+        
         [Test]
         public void Compute_NeverHits99_IsNotFinished()
         {
-            var subject = new IntcodeComputer();
             var input = new List<int> {0,0,0};
-            subject.Compute(input);
-            subject.IsFinished().Should().BeFalse("it should only finish on a 99");
+            _subject.Compute(input);
+            _subject.IsFinished().Should().BeFalse("it should only finish on a 99");
         }
         
         [Test]
         public void Compute_Hits99_IsFinished()
         {
-            var subject = new IntcodeComputer();
             var input = new List<int> {99};
-            subject.Compute(input);
-            subject.IsFinished().Should().BeTrue("because when it hits a 99 it should be finished");
+            _subject.Compute(input);
+            _subject.IsFinished().Should().BeTrue("because when it hits a 99 it should be finished");
         }
     }
 
