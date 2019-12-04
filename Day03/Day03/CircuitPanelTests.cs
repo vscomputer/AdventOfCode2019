6c790d851c79e45768c674d90cd5d66ff84b3f71
Clint Hoagland <bachelor.one@gmail.com>
2019-12-03 21:48:14 -0500

    1st failing test: mark to the right

--- Day03/Day03/CircuitPanelTests.cs
+++ Day03/Day03/CircuitPanelTests.cs
@@ -1,18 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests
-    {
+    public class CircuitPanelTests
+    {        
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void MarkWire1_R8_Marks8PointsToTheRight()
         {
-            Assert.Pass();
+            var subject = new CircuitPanel();
+            subject.MarkWire1("R8");
+            for (int i = 1; i <= 8; i++)
+            {
+                subject.GetWire1().Any(t => t.Item1 == i && t.Item2 == 0).Should().BeTrue();
+            }
+            
+        }
+    }
+
+    public class CircuitPanel
+    {
+        public void MarkWire1(string move)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Tuple<int,int>> GetWire1()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
