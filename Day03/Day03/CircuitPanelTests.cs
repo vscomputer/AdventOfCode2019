e50a6f496db3e6bb6d0ac84c3ac12d71c37957fc
Clint Hoagland <bachelor.one@gmail.com>
2019-12-04 19:49:15 -0500

    now getting manhattan distances

--- Day03/Day03/CircuitPanelTests.cs
+++ Day03/Day03/CircuitPanelTests.cs
@@ -77,6 +77,26 @@ namespace Day03
             _subject.MarkWire2(input2);
             _subject.GetManhattanDistance().Should().Be(6);
         }
+
+        [Test]
+        public void GetManhattanDistance_Scenario2_Returns159()
+        {
+            var input1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+            _subject.MarkWire1(input1);
+            var input2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+            _subject.MarkWire2(input2);
+            _subject.GetManhattanDistance().Should().Be(159);
+        }
+
+        [Test]
+        public void GetManhattanDistance_Scenario3_Returns135()
+        {
+            var input1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
+            _subject.MarkWire1(input1);
+            var input2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+            _subject.MarkWire2(input2);
+            _subject.GetManhattanDistance().Should().Be(135);
+        }
         
     }
 
@@ -218,7 +238,8 @@ namespace Day03
 
         public int GetManhattanDistance()
         {
-            throw new NotImplementedException();
+            return _intersections.Select(position => Math.Abs(position.Item1) + Math.Abs(position.Item2))
+                .Min();
         }
     }
 }
