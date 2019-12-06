00cf13cee64918a0d08c17f1f65a3ee3e503b9be
Clint Hoagland <bachelor.one@gmail.com>
2019-12-05 20:37:36 -0500

    +Day04 boilerplate

---
+++ Day04/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Day04
+{
+    public class GetsPasswordTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
