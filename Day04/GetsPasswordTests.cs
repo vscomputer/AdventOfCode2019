c6fea62d93d09f6d1113db0a6314b40a1086faf1
Clint Hoagland <bachelor.one@gmail.com>
2019-12-05 20:42:38 -0500

    +fluentassertions

---
+++ Day04/GetsPasswordTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace Day04
+{
+    public class GetsPasswordTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+//        [Test]
+//        public void OnlyAscends_Ascending_ReturnsTrue()
+//        {
+//            var subject = new GetsPassword();
+//            subject.OnlyAscends
+//        }
+    }
+
+    public class GetsPassword
+    {
+    }
+}
