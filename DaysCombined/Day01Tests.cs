fc6a6823c3f7cbd87537e9070b7d0cb428902100
Clint Hoagland <bachelor.one@gmail.com>
2020-11-07 19:38:38 -0500

    added logic for new Day 1

--- DaysCombined/Day01Tests.cs
+++ DaysCombined/Day01Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace DaysCombined
@@ -6,10 +8,46 @@ namespace DaysCombined
     [TestFixture]
     public class Day01Tests
     {
+        
+        // For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
         [Test]
-        public void Test1()
+        public void ModuleFuelFinder_CalculateFuel_12_ShouldBe2()
         {
-            Assert.True(true);
+            var subject = new ModuleFuelFinder();
+            var result = subject.CalculateFuel(12);
+            result.Should().Be(2);
+        }
+        
+        //For a mass of 100756, the fuel required is 33583.
+        [Ignore("This is invalid once you take fuel into account")]
+        [Test]
+        public void ModuleFuelFinder_CalculateFuel_100756_ShouldBe33583()
+        {
+            var subject = new ModuleFuelFinder();
+            var result = subject.CalculateFuel(100756);
+            result.Should().Be(33583);
+        }
+
+        [Test]
+        public void ModuleFuelFinder_CalculateFuelWithFuelTakenIntoAccount_1969_ShouldBe966()
+        {
+            var subject = new ModuleFuelFinder();
+            var result = subject.CalculateFuel(1969);
+            result.Should().Be(966);
+        }
+    }
+
+    public class ModuleFuelFinder
+    {
+        public int CalculateFuel(int mass)
+        {
+            int fuelNeeded = 0;
+
+            if (mass <= 0 || (int) (Math.Floor((double) (mass / 3)) - 2) <= 0) return fuelNeeded;
+            mass = (int) (Math.Floor((double) (mass / 3)) - 2);
+            fuelNeeded += mass;
+            return fuelNeeded += CalculateFuel(mass);
+
         }
     }
 }
